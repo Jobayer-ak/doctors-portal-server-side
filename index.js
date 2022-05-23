e9831338208a7f276b161352a00920bb8329bf86
Jobayer-ak <jobayer.ak@gmail.com>
2022-05-23 20:23:05 +0600

    Working on jwt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
@@ -28,7 +29,7 @@ async function run() {
       .collection("bookings");
     const userCollection = client.db("doctors_portal").collection("users");
 
-    //
+    // creating users on server
     app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
       const user = req.body;
@@ -38,7 +39,14 @@ async function run() {
         $set: user,
       };
       const result = await userCollection.updateOne(filter, updateDoc, options);
-      res.send(result);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+          expiresIn: "1h",
+        }
+      );
+      res.send({ result, token });
     });
 
     // get all data
